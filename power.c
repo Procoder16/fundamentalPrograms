0f7805d18c884ee11c3c0a6912fbdfcda3c740ab
Procoder16 <smsoumik1608@gmail.com>
2021-03-26 07:51:14 +0530

    power question in C added

---
+++ power.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+WAP to calculate the exponent value
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   int a=2;
+   
+   int power=2*2*2*2*2;
+   
+   printf("\nThe value is %d.",power);
+   return 0;
+}
+
